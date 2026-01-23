3287219f0f47fe03f555d027aed63ee4e073b928
yasminase <yasminemtimet2@gmail.com>
2026-01-23 02:09:21 +0100

    first try

---
+++ src/pages/login/loginPage.jsx
@@ -0,0 +1,13 @@
+import Card from '../../components/homeCard/card';
+export default function HomePage() {
+  return (
+    <div>
+      <Card header="Welcome Back"
+        postheader="Sign in to your University Companion account"
+        button="Login"
+        Q="Don't have an account? "
+        sign="Sign Up"
+        to="/signup"/>  
+    </div>
+  );
+}
